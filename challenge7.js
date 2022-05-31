444742a4e13868be8fe224b0b5e2cd1f8f5171a0
alam <bagjaalam1@gmail.com>
2022-05-31 13:56:57 +0700

    challenge 7 updated

--- challenge7.js
+++ challenge7.js
@@ -1,18 +1,15 @@
 function weirdMultiply(sentence){
     var numArr = Array.from(String(sentence), Number)
-    
-    if (numArr.length == 1){
+    let total = 1
+    if(numArr.length == 1){
         return numArr[0]
-    } 
-    else if (numArr.length == 2) {
-        
-        return weirdMultiply(numArr[0] * numArr[1])
-    } 
-    else if (numArr.length == 3){
-        return weirdMultiply(numArr[0] * numArr[1] * numArr[2])
+    } else {
+        for(let i=0; i<numArr.length; i++){
+        total *= numArr[i]
+        //console.log(total)
+        } return weirdMultiply(total)
     }
-} 
-
+}
 console.log(weirdMultiply(39))
 console.log(weirdMultiply(999))
 console.log(weirdMultiply(3))
