55b9fa3c8ea5f10a8e75e75e379b8d792c5509c8
alam <bagjaalam1@gmail.com>
2022-06-02 15:54:13 +0700

    challenge 9 done

---
+++ challenge9.js
@@ -0,0 +1,52 @@
+function spiral(param1) {
+    let hasil = []
+    let counter = 0
+
+    for (let i = 0; i < param1; i++) {
+        hasil[i] = []
+        for (let j = 0; j < param1; j++) {
+            hasil[i][j] = counter
+            counter++
+        }
+    }
+    let x = 0
+    let y = 0
+    let result = []
+    let batas = 0
+    let length = param1 * param1
+    while (result.length < length) {
+
+        //ke kanan
+        for (; y < param1; y++) {
+            result.push(hasil[x][y])
+        }
+        x++
+        y--
+
+        //ke bawah
+        for (; x < param1; x++) {
+            result.push(hasil[x][y])
+        }
+        x--
+        y--
+
+        //ke kiri
+        for (; y >= batas; y--) {
+            result.push(hasil[x][y])
+        }
+        x--
+        y++
+
+        //ke atas
+        for (; x > batas; x--) {
+            result.push(hasil[x][y])
+        }
+        x++
+        y++
+        batas++
+        param1--
+    }
+    console.log(result)
+}
+spiral(5)
+spiral(6) 
