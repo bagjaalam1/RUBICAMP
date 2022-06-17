0e7b18f47b2a8b1e182b508b5f86afa102efa394
bagjaalam1 <bagjaalam1@gmail.com>
2022-06-17 11:29:26 +0700

    challenge 15 done

--- challenge15.sql
+++ challenge15.sql
@@ -2,7 +2,16 @@ select mahasiswa.nim, mahasiswa.nama, mahasiswa.umur, mahasiswa.alamat, jurusan.
 
 select mahasiswa.nama, mahasiswa.umur from mahasiswa where mahasiswa.umur < 20;
 
-select mahasiswa.nama, kontrak.nilai from mahasiswa join kontrak on mahasiswa.nim = kontrak.nim where not kontrak.nilai = 'C' and kontrak.nilai != 'B' and kontrak.nilai != 'D' and kontrak.nilai != 'F';
+select mahasiswa.nama, kontrak.nilai from mahasiswa join kontrak on mahasiswa.nim = kontrak.nim where kontrak.nilai = 'A';
 
 select mahasiswa.nama, matakuliah.sks from mahasiswa join kontrak on mahasiswa.nim = kontrak.nim join matakuliah on matakuliah.kodematkul = kontrak.kodematkul where matakuliah.sks > 10;
 
+select mahasiswa.nama, matakuliah.nama from mahasiswa join kontrak on mahasiswa.nim = kontrak.nim join matakuliah on matakuliah.kodematkul = kontrak.kodematkul where matakuliah.kodematkul = 'M0003';
+
+select count(mahasiswa.nama), dosen.namadosen from mahasiswa join kontrak on mahasiswa.nim = kontrak.nim join dosen on dosen.nidn = kontrak.nidn group by dosen.nidn;
+
+select nama, umur from mahasiswa order by umur asc;
+
+select kontrak.id, kontrak.nim, kontrak.kodematkul, kontrak.nilai from kontrak where kontrak.nilai = 'D' or kontrak.nilai = 'E';
+
+select mahasiswa.nama, mahasiswa.nim, mahasiswa.alamat, jurusan.namajurusan, jurusan.kodejurusan, kontrak.nilai, dosen.namadosen, dosen.nidn from mahasiswa join jurusan on jurusan.kodejurusan = mahasiswa.kodejurusan join kontrak on mahasiswa.nim = kontrak.nim join dosen on dosen.nidn = kontrak.nidn;
